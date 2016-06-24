a507796426961e752b8db24d1990fc8adb6ed8a9
Divyang <divyang.duhan07@gmail.com>
2016-06-24 18:22:51 +0530

    current places partially working

--- app/src/main/java/com/devil07/divyang/gplaces/MainActivity.java
+++ app/src/main/java/com/devil07/divyang/gplaces/MainActivity.java
@@ -1,11 +1,20 @@
 package com.devil07.divyang.gplaces;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.StringDef;
+import android.support.v4.app.ActivityCompat;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -13,26 +22,39 @@ import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlacePicker;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
     private GoogleApiClient mGoogleApiClient;
-    Button bt1;
+    Button bt1, bt2;
     int PLACE_PICKER_REQUEST = 1;
+    private static final int GOOGLE_API_CLIENT_ID = 0;
+    TextView tv1;
+    private static final String TAG = "MainActivity";
+    String rom=" ";
+    private static final int PERMISSION_REQUEST_CODE = 100;
+    PendingResult<PlaceLikelihoodBuffer> result;
+    private boolean permissionGranted=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bt1 = (Button) findViewById(R.id.bt1);
+        bt2 = (Button) findViewById(R.id.bt2);
+
 
         mGoogleApiClient = new GoogleApiClient
                 .Builder(this)
                 .addApi(Places.GEO_DATA_API)
                 .addApi(Places.PLACE_DETECTION_API)
-                .enableAutoManage(this, this)
+                .enableAutoManage(this,GOOGLE_API_CLIENT_ID, this)
                 .build();
 
 
@@ -54,10 +76,118 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         });
 
 
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            PERMISSION_REQUEST_CODE);
+                }
+                else {
+                    permissionGranted=true;
+                }
+                callapi();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*                if(mGoogleApiClient.isConnected()) {
+
+
+                    if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                            ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                        Toast.makeText(MainActivity.this, "in if", Toast.LENGTH_SHORT).show();
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                PERMISSION_REQUEST_CODE);
+
+
+
+                    } else {
+
+                        Toast.makeText(MainActivity.this, "in else", Toast.LENGTH_SHORT).show();
+                        callapi();
+                        tv1.setText(rom);
+
+                    }
+
+
+                }*/
+
+
+
+
+
+            }
+        });
+
+
     }
+
+
+
+    private void callapi()throws SecurityException {
+
+      PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
+          .getCurrentPlace(mGoogleApiClient, null);
+      result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+        @Override
+        public void onResult(@NonNull PlaceLikelihoodBuffer likelyPlaces) {
+             Toast.makeText(MainActivity.this,"out for",Toast.LENGTH_SHORT).show();
+
+            for(int i=0;i<likelyPlaces.getCount();i++)
+            {
+               // Toast.makeText(MainActivity.this,"in for",Toast.LENGTH_SHORT).show();
+                    Place pla =likelyPlaces.get(i).getPlace();
+
+//                    rom=pla.getName().toString();
+                Toast.makeText(MainActivity.this,"Place-"+rom,Toast.LENGTH_SHORT).show();
+
+            }
+          likelyPlaces.release();
+        }
+      });
+
+
+    }
+
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+
+
+
+
+
+
+
+
+         Log.e(TAG, "Google Places API connection failed with error code: "
+                        + connectionResult.getErrorCode());
+
+                Toast.makeText(this,
+                        "Google Places API connection failed with error code:" +
+                                connectionResult.getErrorCode(),
+                        Toast.LENGTH_LONG).show();
+
 
     }
 
@@ -72,5 +202,77 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                 Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
             }
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        Toast.makeText(MainActivity.this,"in request"+requestCode,Toast.LENGTH_SHORT).show();
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    permissionGranted=true;
+
+                   Toast.makeText(MainActivity.this,"Granted",Toast.LENGTH_SHORT).show();
+                    callapi();
+                }
+                else {
+
+                    Toast.makeText(MainActivity.this,"not Granted",Toast.LENGTH_SHORT).show();
+
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mGoogleApiClient.connect();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        mGoogleApiClient.disconnect();
+    }
+
+    public void requestPermission()
+    {
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISSION_REQUEST_CODE);
+            }
+            else {
+                permissionGranted=true;
+            }
+
+
+            return;
+        }
+
+
+
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        if(permissionGranted) {
+            if (mGoogleApiClient.isConnected()) {
+                callapi();
+            }
+        }
+
     }
 }
